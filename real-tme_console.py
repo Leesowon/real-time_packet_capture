f8667f91fc2050fe338a9cfd5d6c555465f2c0a8
sowon <leesowon00@naver.com>
2023-10-20 11:06:55 +0900

    23-10-20 use pkt_callback function

--- real-tme_console.py
+++ real-tme_console.py
@@ -1,23 +1,41 @@
 import pyshark
-import time
+
+def pkt_callback(pkt):
+    if 'mqtt' in pkt:
+        try:
+            timestamp = pkt.sniff_timestamp
+            source = pkt.ip.src
+            destination = pkt.ip.dst
+            mqtt_topic = pkt.mqtt.topic
+            mqtt_payload = pkt.mqtt.payload.decode('utf-8')
+
+            # MQTT 패킷 정보를 콘솔에 출력
+            print(f'Timestamp: {timestamp}, Source: {source}, Destination: {destination}, MQTT Topic: {mqtt_topic}, MQTT Payload: {mqtt_payload}')
+        except AttributeError:
+            # MQTT 패킷이 아닌 경우 AttributeError가 발생할 수 있음
+            pass
 
 # MQTT 패킷을 캡처할 네트워크 인터페이스 선택
-# network_interface = 'eth0'  # 이더넷 인터페이스
-network_interface = 'Adapter for loopback traffic capture'
+network_interface = 'Adapter for loopback traffic capture'  # 무선 LAN 인터페이스
 
+# 패킷 캡처
+capture = pyshark.LiveCapture(interface=network_interface, bpf_filter='mqtt')
+
+# 캡처된 패킷에 대해 pkt_callback 함수를 적용
+capture.apply_on_packets(pkt_callback)
+
+
+'''
 # 패킷 캡처와 MQTT 패킷 정보 출력
 # capture = pyshark.LiveCapture(interface=network_interface, bpf_filter='MQTT')
 capture = pyshark.LiveCapture(interface=network_interface, bpf_filter = 'tcp')
 
-'''
 capture = pyshark.LiveCapture(interface=network_interface,
     bpf_filter='tcp port 1883',
     use_json=True, include_raw=True) 
-'''
-cnt = 0
 
 for packet in capture.sniff_continuously():
-    '''
+    
     print("loop", cnt)
     cnt = cnt+1
     
@@ -26,8 +44,7 @@ for packet in capture.sniff_continuously():
     dstport = int(packet.tcp.dstport)
     if dstport != 1883: continue
     print(f'{packet.ip.src}:{srcport} -> {packet.ip.dst}:{dstport}')
-    '''
-
+   
     if ('tcp' not in packet) or ('mqtt' not in packet.layers):
         continue
     
@@ -43,3 +60,4 @@ for packet in capture.sniff_continuously():
     except AttributeError:
         # MQTT 패킷이 아닌 경우 AttributeError가 발생할 수 있음
         pass
+        '''
